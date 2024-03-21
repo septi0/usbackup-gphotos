1265b42aa73ac30a35cfa3f4abee90f9e12f3dc2
Septi <septi0@users.noreply.github.com>
2024-03-21 19:02:46 +0200

    traceback for exception

--- usbackup_gphotos/media_items.py
+++ usbackup_gphotos/media_items.py
@@ -383,7 +383,8 @@ class MediaItems:
         # update items status based on async tasks results
         for t in tasks:
             if t.exception():
-                self._logger.error(f'Sync for media item #{t.get_name()} failed. {t.exception()}')
+                self._logger.error(f'Sync for media item #{t.get_name()} failed')
+                self._logger.debug(t.exception(), exc_info=True)
                 self._model.update_media_item_meta(t.get_name(), status='sync_error')
 
                 info.increment(failed=1)
